b1f2e14495dd2ef278f3e35a840f91af4128b0a8
Stephen Lewis <stlewis@udel.edu>
2024-02-22 17:36:20 -0500

    First  2 functions

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const publish = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    return publish;
 }
 
 /**
@@ -15,7 +18,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const non_empty = questions.filter(
+        (question: Question): boolean =>
+            question.body.length !== 0 ||
+            question.expected.length !== 0 ||
+            question.options.length !== 0
+    );
+    return non_empty;
 }
 
 /***
