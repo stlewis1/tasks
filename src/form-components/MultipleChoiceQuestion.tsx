64218ecbde5c09b63a2264e58dc21440cf6a09a8
Stephen Lewis <stlewis@udel.edu>
2024-03-04 19:43:04 -0500

    Finished Multiple Choice Question

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FormGroup, FormLabel, FormSelect } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,23 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <FormGroup controlId="answerOptions">
+                <FormSelect value={choice} onChange={updateChoice}>
+                    {options.map((o: string) => (
+                        <option key={o} value={o}>
+                            {o}
+                        </option>
+                    ))}
+                </FormSelect>
+            </FormGroup>
+            {expectedAnswer === choice ? "✔️" : "❌"}
         </div>
     );
 }
